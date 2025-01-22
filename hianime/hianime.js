e6e34e65f7e934e2d8083298cd8ff8f6433430ef
50/50 <80717571+50n50@users.noreply.github.com>
2025-01-22 14:27:53 +0400

    Update hianime.js

--- hianime/hianime.js
+++ hianime/hianime.js
@@ -22,7 +22,6 @@ async function searchResults(keyword) {
 async function extractDetails(url) {
     try {
         const html = await fetch(url);
-        console.log(html);
 
         const descriptionMatch = html.match(/<div class="film-description m-hide">\s*<div class="text">\s*<p>([\s\S]*?)<\/p>\s*<\/div>/);
         const durationMatch = html.match(/<span class="item">(\d+m)<\/span>/);
