76ef3e88e7274ce0f5c8eb7d2cd798fcca3f6569
50/50 <80717571+50n50@users.noreply.github.com>
2025-02-07 19:01:47 +0400

    Update fireanime.js

--- fireanime/fireanime.js
+++ fireanime/fireanime.js
@@ -2,7 +2,7 @@ async function searchResults(keyword) {
     try {
         const encodedKeyword = encodeURIComponent(keyword);
         const responseText = await fetch(`https://fireani.me/api/anime/search?q=${encodedKeyword}`);
-        const data = JSON.parse(responseText);
+        const data = await JSON.parse(responseText);
         
         const transformedResults = data.data.map(anime => ({
             title: anime.title,
@@ -22,7 +22,7 @@ async function extractDetails(slug) {
     try {
         const encodedID = encodeURIComponent(slug);
         const response = await fetch(`https://fireani.me/api/anime?slug=${encodedID}`);
-        const data = JSON.parse(response);
+        const data = await JSON.parse(response);
         
         const animeInfo = data.data;
         
@@ -44,50 +44,58 @@ async function extractDetails(slug) {
 }
 
 async function extractEpisodes(slug) {
-  try {
-    const encodedID = encodeURIComponent(slug);
-    const response = await fetch(`https://fireani.me/api/anime?slug=${encodedID}`);
-    const data = JSON.parse(await response._bodyInit);
-      
-    const episodes = data.data.anime_seasons[0]?.anime_episodes || [];
-      
-    const transformedResults = episodes.map(episode => ({
-      href: `${encodedID}&episode=${episode.episode}`,
-      number: episode.episode
-    }));
-      
-    return JSON.stringify(transformedResults);
-  } catch (error) {
-    console.log('Fetch error:', error);
-  }
+    try {
+        const encodedID = encodeURIComponent(slug);
+        const response = await fetch(`https://fireani.me/api/anime?slug=${encodedID}`);
+        const data = await JSON.parse(response);
+
+        const episodes = data.data.anime_seasons.reduce((acc, season, seasonIndex) => {
+            const seasonNumber = seasonIndex + 1; 
+            const seasonEpisodes = season.anime_episodes || [];
+            seasonEpisodes.forEach(episode => {
+                acc.push({
+                    href: `${encodedID}&season=${seasonNumber}&episode=${episode.episode}`,
+                    number: episode.episode
+                });
+            });
+            return acc;
+        }, []);
+
+        console.log(episodes);
+        return JSON.stringify(episodes);
+    } catch (error) {
+        console.log('Fetch error:', error);
+    }    
 }
 
+
+
+extractEpisodes('naruto-shippuden');
+
 async function extractStreamUrl(id) {
-  try {
-    const encodedID = encodeURIComponent(id);
-    const response = await fetch(`https://fireani.me/api/anime/episode?slug=${encodedID}`);
-    const data = JSON.parse(await response._bodyInit);
-      
-    const voeStream = data.data.anime_episode_links.find(link => link.name === 'VOE' && link.lang === 'eng-sub');
-      
-    if (voeStream) {
-      const newLink = voeStream.link.replace('https://voe.sx/e/', 'https://maxfinishseveral.com/e/');
-      const tempHTML = await fetch(newLink);
-      const tempHTMLText = await tempHTML._bodyInit;
-        
-      const hlsMatch = tempHTMLText.match(/var\s+sources\s*=\s*({.*?})/s);
-        
-      if (hlsMatch) {
-        const sources = JSON.parse(hlsMatch[1]);
-        const hlsEncodedUrl = sources.hls;
-        const decodedUrl = atob(hlsEncodedUrl);
-        return decodedUrl;
-      }
+    try {
+        const encodedID = encodeURIComponent(id);
+        const response = await fetch(`https://fireani.me/api/anime/episode?slug=${encodedID}`);
+        const data = JSON.parse(response);
+       
+       const voeStream = data.data.anime_episode_links.find(link => link.name === 'VOE' && link.lang === 'eng-sub');
+
+       if (voeStream) {
+        const newLink = voeStream.link.replace('https://voe.sx/e/', 'https://maxfinishseveral.com/e/');
+        const tempHTML = await fetch(newLink);
+
+        const hlsMatch = tempHTML.match(/var\s+sources\s*=\s*({.*?})/s);
+        if (hlsMatch) {
+            const sources = JSON.parse(hlsMatch[1]);
+            const hlsEncodedUrl = sources.hls;
+
+            const decodedUrl = atob(hlsEncodedUrl);
+            return decodedUrl;
+        }
     }
-      
-    return null;
-  } catch (error) {
-    console.log('Fetch error:', error);
-    return null;
-  }
+        return null;
+    } catch (error) {
+       console.log('Fetch error:', error);
+       return null;
+    }
 }
