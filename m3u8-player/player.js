df4c0083c18a371713da2c216f752bb785ed7cb2
moaadhzair <92125533+moaadhzair@users.noreply.github.com>
2025-04-15 11:26:35 +0100

    Update player.js

--- m3u8-player/player.js
+++ m3u8-player/player.js
@@ -126,7 +126,7 @@ async function extractEpisodes(id) {
 
 async function extractStreamUrl(url) {
   const urls = [];
-  urls.push("http://playertest.longtailvideo.com/adaptive/wowzaid3/playlist.m3u8");
+  urls.push("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8");
   urls.push("http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8");
   
   return urls;
